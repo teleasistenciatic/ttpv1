3409625c8abb84ec4520148ad3b6ed70a3d03c8a
teleasistenciatic <teleasistenciatic@gmail.com>
2015-03-18 11:04:20 +0100

    actDebugCifrado

--- app/src/main/java/com/local/android/teleasistenciaticplus/act/debug/actMainDebug.java
+++ app/src/main/java/com/local/android/teleasistenciaticplus/act/debug/actMainDebug.java
@@ -37,7 +37,7 @@ public class actMainDebug extends ActionBarActivity {
                     "Configuración usuario",// 0,
                     "Envío SMS", //id 1
                     "Lectura .LOG", //id 2
-                    "Encriptacion/Desencriptación", //id 3
+                    "Cifrado/Descifrado", //id 3
                     "Comprobacion conexión a servidor", // id 4
                     "Acceso de datos e internet", //id 5
                     "Uso de la memoria", //id 6
@@ -70,6 +70,9 @@ public class actMainDebug extends ActionBarActivity {
                             break;
                         case 4: //"Comprobacion conexión a servidor"
                             actToLoad = actDebugCheckServer.class;
+                            break;
+                        case 3: //"Cifrado Descifrado"
+                            actToLoad = actDebugCifrado.class;
                             break;
                     }
 
@@ -139,56 +142,5 @@ public class actMainDebug extends ActionBarActivity {
     public void exit_button(View view) {
         finish();
     }
-
-    /*
-
-    public void main_debug_button_cifrado_test(View view) throws Exception {
-
-
-        //Obtenemos el texto de la caja de texto
-        TextView textEditConTextoACifrar = (TextView) findViewById(R.id.edit_string_cifrado_input);
-        String textoACifrar = textEditConTextoACifrar.getText().toString();
-
-        if (textoACifrar.isEmpty()) {
-            Toast.makeText(this, getResources().getString(R.string.error_empty_string), Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        //Llamamos a la encriptación
-        Cifrado miCifrado = new Cifrado();
-        String cifrado = miCifrado.cifrar(textoACifrar);
-        ///////////////////////////////
-
-        //Obtenemos la caja de texto de salida
-        TextView textEditConCifrado = (TextView) findViewById(R.id.edit_string_cifrado_output);
-        textEditConCifrado.setText(cifrado);
-
-        AppLog.i(">>actMainDebug<< texto cifrado", cifrado);
-    }
-
-    public void main_debug_button_descifrado_test(View view) throws Exception {
-
-        //Obtenemos el texto de la caja de texto
-        TextView textEditConTextoADescifrar = (TextView) findViewById(R.id.edit_string_cifrado_input);
-        String textoADescifrar = textEditConTextoADescifrar.getText().toString();
-
-        if (textoADescifrar.isEmpty()) {
-            Toast.makeText(this, getResources().getString(R.string.error_empty_string), Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        //Llamamos a la desencriptación
-        Cifrado miCifrado = new Cifrado();
-        String descifrado = miCifrado.descifrar(textoADescifrar);
-        ///////////////////////////////
-
-        //Obtenemos la caja de texto de salida
-        TextView textEditConDescifrado = (TextView) findViewById(R.id.edit_string_cifrado_output);
-        textEditConDescifrado.setText(descifrado);
-
-        AppLog.i(">>actMainDebug<< texto descrifrado", descifrado);
-    }
-
-    */
 
 } // Fin actividadMainDebug
