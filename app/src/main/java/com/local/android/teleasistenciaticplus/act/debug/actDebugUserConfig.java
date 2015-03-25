07759140c431ce7f19c2a129a49b53068a2c0db2
teleasistenciatic <teleasistenciatic@gmail.com>
2015-03-25 12:41:35 +0100

    Actividad de depuración para poder borrar las SharedPreferences

--- app/src/main/java/com/local/android/teleasistenciaticplus/act/debug/actDebugUserConfig.java
+++ app/src/main/java/com/local/android/teleasistenciaticplus/act/debug/actDebugUserConfig.java
@@ -2,8 +2,6 @@ package com.local.android.teleasistenciaticplus.act.debug;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.local.android.teleasistenciaticplus.R;
 
